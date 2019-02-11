98512ba130bdcaaf1e86687e142d2ad73e22ca5c
UMULISA12 <umulisaa0@gmail.com>
2019-02-11 12:10:58 +0200

    creating classes

---
+++ password.py
@@ -0,0 +1,29 @@
+   class Users:
+    """
+    Class that generates new instances of contacts
+
+    """
+  
+    def __init__(self,first_name,last_name,user_name,password):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+
+
+    class Credentials:
+    """
+    Class that generates credentials
+    """
+    def __init__(self,first_name,last_name,user_name,password):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
