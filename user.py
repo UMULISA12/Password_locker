c504922a19b424d72824a174ee25ec3fa4dd724b
UMULISA12 <umulisaa0@gmail.com>
2019-02-11 12:39:44 +0200

    creating test file

---
+++ user.py
@@ -0,0 +1,19 @@
+class Users:
+    """
+    Class that generates new instances of contacts
+
+    """
+    username_and_password_list = [] # Empty credential list
+  
+    def __init__(self,first_name,last_name,user_name,password):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+
+
+       
