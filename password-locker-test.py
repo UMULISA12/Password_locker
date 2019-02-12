495f7c9f18245133b7550c8f5b4daf3c3b1ce1b1
UMULISA12 <umulisaa0@gmail.com>
2019-02-12 21:56:43 +0200

    working on test

--- password-locker-test.py
+++ password-locker-test.py
@@ -1,165 +1,57 @@
 import unittest # Importing the unittest module
-from user import Users # Importing the contact class
-from credential import Credentials
+from user import User # import user class
 
-
-class TestUsers(unittest.TestCase):
-
+class TestUser(unittest.TestCase):
     '''
-    Test class that defines test cases for the users class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
+    Test class that defines test cases for the user class behaviours
     '''
-   
+
+    def tearDown(self):
+            '''
+            tearDown method that does cleanup after each test case has run
+            '''
+            User.user_list = []
+
     def setUp(self):
         '''
-        Set up method to run before each test cases.
+        Set up method to run before each test cases .
         '''
-        self.new_user = Users("Alice","UMULISA","Alicia","abana") # create user object
-        # self.new_credential = Credentials("Github","UMULISA12","abana") # creating credential object
-
+        self.new_user = User("Alice","12")
 
     def test_init(self):
         '''
-        test_init test case to test if the user object is initialized properly
+        test_init test case to test if the object is initialized properly
         '''
-
-        self.assertEqual(self.new_user.first_name,"Alice")
-        self.assertEqual(self.new_user.last_name,"UMULISA")
-        self.assertEqual(self.new_user.user_name,"Alicia")
-        self.assertEqual(self.new_user.password,"abana")
+        self.assertEqual(self.new_user.first_name,"Alice)
+        self.assertEqual(self.new_user.user_password,"12")
+        self.assertEqual(self.new_credentials.site_name,"Github")
+        self.assertEqual(self.new_credentials.site_username,"Alicia")
+        self.assertEqual(self.new_credentials.site_password,"123")
 
     def test_save_user(self):
         '''
-        test_save_user test case to test if the user object is saved into
-         the user list
+        test case to test if the user object is saved
         '''
-        self.new_user.save_user() # saving the new contact
-        self.assertEqual(len(Users.username_and_password_list),1)
+        self.new_user.save_user() # save a user
+        self.assertEqual(len(User.user_list),1)
 
-
-    # setup and class creation up here
-    def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
-            Users.username_and_password_list = []
-    
-    
-    
-    def test_save_multiple_user(self):
+    def test_multiple_user(self):
         '''
-        test_save_multiple_users to check if we can save multiple user
-         objects to our user_list
+        test case to check if we can save multiple user objects
         '''
         self.new_user.save_user()
-        test_user = Users("Alice","UMULISA","Alicia","abana") # new contact
+        test_user = User("Test","user")
         test_user.save_user()
-        self.assertEqual(len(Users.username_and_password_list),2)
+        self.assertEqual(len(User.user_list),2)
 
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-
-
-class TestCredentials(unittest.TestCase):
-    '''
-    Set up method to run before each test cases.
-    '''
-
-    def setUp(self):
-        # '''
-        # Set up method to run before each test cases.
-        # '''
-        # self.new_user = Users("Alice","UMULISA","Alicia","abana")
-     self.new_credential = Credentials("Github","UMULISA12","abana") # creating credential object
-    
-
-    def test_init(self):
+    @classmethod
+    def test_display_credentials(cls,name,password):
         '''
-        test_init test case to test if the credential object is initialized properly
+        test case to test if we can display credentials by name and password
         '''
-
-        self.assertEqual(self.new_credential.site_name,"Github")
-        self.assertEqual(self.new_credential.site_username,"UMULISA12")
-        self.assertEqual(self.new_credential.site_password,"abana")
-
-
-    def test_save_credential(self):
-        '''
-        test_save_credential test case to test if the credential object is saved into
-         the credential list
-        '''
-        self.new_credential.save_credential() # saving the new credential
-        self.assertEqual(len(Credentials.credentials_list),1)
-
-
-
-    # setup and class creation up here
-    def tearDown(self):
-        '''
-        tearDown method that does clean up after each test case has run.
-        '''
-        Credentials.credentials_list = []
-
-
-
-    def test_save_multiple_credential(self):
-        '''
-        test_save_multiple_credentials to check if we can save multiple credential
-         objects to our credentials_list
-        '''
-        self.new_credential.save_credential()
-        test_credential = Credentials("Github","UMULISA12","abana") # new credential
-        test_credential.save_credential()
-        self.assertEqual(len(Credentials.credentials_list),2)
-
-
-
-  
-    def test_delete_credential(self):
-            '''
-            test_delete_credential to test if we can remove a credential from our credentials list
-            '''
-            self.new_credential.save_credential()
-            test_credential = Credentials("Github","UMULISA12","abana") # new credential
-            test_credential.save_credential()
-
-            self.new_credential.delete_credential()# Deleting a contact object
-            self.assertEqual(len(Credentials.credentials_list),1)
-
-
-
-    def test_find_credential_by_site_name(self):
-            '''
-            test to check if we can find credential by site_name and display username and password
-            '''
-
-            self.new_credential.save_credential()
-            test_credential = Credentials("Github","UMULISA12","abana") # new credential
-            test_credential.save_credential()
-
-            found_credential = Credentials.find_by_site_name("Github")
-
-            self.assertEqual(found_credential.site_username,test_credential.site_username)
-
-
-
-
-
-
-
-
-
-
-
-
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
